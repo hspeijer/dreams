c46cc71c4225f7eb02721d0e0c3159bc4ab427ee
Hans Speijer <hans.speijer@mindsteps.eu>
2017-03-16 13:15:07 +0100

    Fix admin listing

--- app/admin/camps.rb
+++ app/admin/camps.rb
@@ -1,7 +1,7 @@
 ActiveAdmin.register Camp do
   EXCLUDED = %w(contact_email user_id seeking_members safetybag_firstMemberName safetybag_firstMemberEmail safetybag_secondMemberName safetybag_secondMemberEmail)
 
-  scope :displayed_with_tags, default: true
+  # scope :displayed_with_tags, default: true
   remove_filter *%i(tag_taggings taggings base_tags)
   index do
     selectable_column
